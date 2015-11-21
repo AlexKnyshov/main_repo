22dabc97db86ea28df135b7604ee8d32edf47b4b
AlexKnyshov <alexknyshov@gmail.com>
2015-11-21 09:37:46 -0800

    version 0.3 corrected the matrix, length calc

--- distances.py
+++ distances.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO
 filename = raw_input("enter the filename")
 infile = open(filename, "r")
+#infile = open("./fasta/T58_L1.phylip.fas", "r")
 seqs = {}
 for seq in SeqIO.parse(infile, "fasta"):
     seqs[seq.id] = str(seq.seq)
@@ -9,9 +10,10 @@ names =[]
 for key in seqs.keys():
 	names.append(key)
 distlist = []
-matrix = [[0 for x in range(len(names)-1)] for y in range(len(names)-1)] #just for visualization
+matrix = [[0 for x in range(len(names))] for y in range(len(names))] #just for visualization
 row = 0
 inter = 0
+labelvert = False
 for num in range(len(names)-1):
 	pair1 = []
 	pair1 += seqs[names[num]]
@@ -64,16 +66,21 @@ for num in range(len(names)-1):
 			elif pair2[index] == "-" and pair1[index] != "-":
 				counterindel +=1
 		#calculating the distance
-		val = round((float(counter)/(endpos-startpos)*100), 2)
-		matrix[inter][row] = val
+		val = round((float(counter)/(endpos-startpos+1)*100), 2) #transversions only
+		#val = (float(counter+countert+counterindel)/(endpos-startpos+1))**0.5 #sqrt of normal p-distance
+		matrix[inter+row][row] = val
+		if labelvert == False:
+			matrix[inter][len(names)-1] = names[p2][6:15]
 		distlist.append(val)
 		inter += 1
+	matrix[len(names)-1][row] = names[num][6:15]
 	row +=1
+	labelvert = True
 	inter = 0
 print "distance matrix (per cent)"
 for r1 in matrix:
 	for r2 in r1:
-		print '{:4}'.format(r2),
+		print '{:9}'.format(r2),
 	print
 print "average distance", sum(distlist)/len(distlist)
 infile.close()
