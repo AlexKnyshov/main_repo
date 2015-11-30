0d54f89d38008440dfee5102350f2fa9af9fcdb2
AlexKnyshov <alexknyshov@gmail.com>
2015-11-29 19:29:52 -0800

    fixed comments

--- distances.py
+++ distances.py
@@ -2,7 +2,6 @@ from Bio import SeqIO
 import glob
 import sys
 import os
-from fnmatch import fnmatch
 import operator
 #filepath input
 if len(sys.argv) > 1:
@@ -13,7 +12,7 @@ else:
 	sys.exit()
 if len(files) == 0:
 	print "no fasta files in the directory"
-#starting processing files
+#starting to process files
 loci = {}
 progbarc = 0
 for f in files:
@@ -27,17 +26,12 @@ for f in files:
 	for key in seqs.keys():
 		names.append(key)
 	distlist = []
-	#matrix = [[0 for x in range(len(names))] for y in range(len(names))] #just for visualization
-	#row = 0
-	#inter = 0
-	#labelvert = False
 	for num in range(len(names)-1):
 		pair1 = []
 		pair1 += seqs[names[num]]
 		for p2 in range(num+1, len(names)):
 			pair2 = []
 			pair2 += seqs[names[p2]]
-			#print names[num], names[p2]
 			counter = 0
 			countert = 0
 			counterindel = 0
@@ -57,7 +51,6 @@ for f in files:
 					needskip = False
 					break
 			if needskip:
-				#print "pair", names[num], names[p2], "skipped"
 				val = "N/A"
 			else:
 				#checking the differences
@@ -91,34 +84,22 @@ for f in files:
 					elif pair2[index] == "-" and pair1[index] != "-":
 						counterindel +=1
 				#calculating the distance
-				val = round((float(counter+countert+counterindel)/(endpos-startpos+1)*100), 2) #transversions only, percent
+				val = round((float(counter+countert+counterindel)/(endpos-startpos+1)*100), 2) #1-1-5 costmatrix, percent
 				#val = (float(counter+countert+counterindel)/(endpos-startpos+1)) #normal p-distance as in R package {ape} with pairwise deletion except weird indel counter
-			#matrix[inter+row][row] = val
-			# if labelvert == False:
-			# 	matrix[inter][len(names)-1] = names[p2][6:15]
 			if val != "N/A":
 				distlist.append(val)
-			#inter += 1
-		# matrix[len(names)-1][row] = names[num][6:15]
-		# row +=1
-		# labelvert = True
-		# inter = 0
-	# print "distance matrix (per cent)"
-	# for r1 in matrix:
-	# 	for r2 in r1:
-	# 		print '{:9}'.format(r2),
-	#  	print
 	print "average distance", sum(distlist)/len(distlist)
 	fname = f.split("/")
 	locus = fname[2].split(".")[0]
 	loci[locus] = sum(distlist)/len(distlist)
 	infile.close()
+	#progress bar
 	progbarc +=1
 	progbar = int(round(float(progbarc)/len(files)*100, 0))
 	hashes = '#' * int(progbar * 0.2)
 	spaces = ' ' * (20 - len(hashes))
-	sys.stdout.write("\rProgress: [{0}] {1}%".format(hashes + spaces, progbar))
-	sys.stdout.flush()
+	print "\rProgress: [{0}] {1}%".format(hashes + spaces, progbar)
+#final table output
 with open("loci.tab", "w") as outfile:
 	for lc, lcv in sorted(loci.items(), key=operator.itemgetter(1)):
 		print >> outfile, lc, "\t", lcv
