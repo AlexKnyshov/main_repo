95f42d96ff1273979b1cce113591ece74f8b01fa
AlexKnyshov <alexknyshov@gmail.com>
2015-11-30 11:56:35 -0800

    fifth

--- starter.sh
+++ starter.sh
@@ -1,3 +1,3 @@
-qsub -l nodes=1:ppn=8
-python treeanalysis.py ./original_data 50
+#qsub -l nodes=1:ppn=8
+qsub -S $(which python) -l nodes=1:ppn=8 treeanalysis.py ./original_data 50
 #echo ‘myscript.sh prot.faa 2’ | qsub
