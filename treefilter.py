121527b32974ac25327f0bb28c05f51d7c6ac41a
AlexKnyshov <alexknyshov@gmail.com>
2015-12-03 12:14:49 -0800

    minor fix

--- treefilter.py
+++ treefilter.py
@@ -37,7 +37,7 @@ with open(filename, "rb") as csvfile:
 	reader = csv.reader(csvfile, delimiter='\t')
 	for row in reader:
 		#print row[0], row[1]
-		loci[row[0]] = float(row[1])
+		loci[row[0].strip()] = float(row[1])
 		values.append(float(row[1]))
 mn = min(values)
 mx = max(values)
