2d9cd5d75bf7184e09484302bc828d8ae5567fdc
AlexKnyshov <alexknyshov@gmail.com>
2015-12-02 19:42:55 -0800

    separate raxml script

---
+++ treegen.sh
@@ -0,0 +1,2 @@
+module load RAxML/8.2.3
+raxmlHPC-PTHREADS-SSE3 -T 16 --silent -f a -c 25 -p 12345 -x 12345 -m GTRCAT -n $(sed -n ${PBS_ARRAYID}p filelist.lst).tre -N 100 -s $(sed -n ${PBS_ARRAYID}p filelist.lst).phylip
