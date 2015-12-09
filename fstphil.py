315bc0294ba41b924a6302274b426b95d2fdc1ff
AlexKnyshov <alexknyshov@gmail.com>
2015-12-08 17:52:32 -0800

    modifications

--- fstphil.py
+++ fstphil.py
@@ -1,12 +1,19 @@
-from Bio import SeqIO
+from Bio import AlignIO
 import sys
 import glob
 import os
-inputfolder = sys.argv[1]
-inputformat = sys.argv[2]
-inputext = sys.argv[3]
-outputfolder = sys.argv[4]
-outputformat = sys.argv[5]
+if len(sys.argv) == 7:
+	inputfolder = sys.argv[1]
+	inputformat = sys.argv[2]
+	inputext = sys.argv[3]
+	outputfolder = sys.argv[4]
+	outputformat = sys.argv[5]
+	outputext = sys.argv[6]
+else:
+	print "FORMAT: python fstphil.py [inputfolder] [inputformat] [inputext] [outputfolder] [outputformat] [outputext]"
+	print "EXAMPLE: python fstphil.py ./fasta fasta fas ./phylip phylip-relaxed phylip"
+	sys.exit()
+
 files = glob.glob(inputfolder+"/*."+inputext)
 extlen = len(inputext)
 if not os.path.exists ("./"+outputfolder):
@@ -15,7 +22,11 @@ for f in files:
 	fnew = f.split("/")
 	fn = fnew[len(fnew)-1]
 	print fn[:(len(fn)-extlen)]
-	print outputfolder+"/"+fn[:(len(fn)-extlen)]+"phylip"
-	count = SeqIO.convert(f, inputformat, outputfolder+"/"+fn[:(len(fn)-extlen)]+"phylip", outputformat)
+	print outputfolder+"/"+fn[:(len(fn)-extlen)]+outputext
+	alignments = AlignIO.parse(f, inputformat)
+	AlignIO.write(alignments, outputfolder+"/"+fn[:(len(fn)-extlen)]+outputext, outputformat)
+	output_handle.close()
+	input_handle.close()
+	#count = SeqIO.convert(f, inputformat, outputfolder+"/"+fn[:(len(fn)-extlen)]+"phylip", outputformat)
 #print "Converted %i records" % count
 print "Done"
