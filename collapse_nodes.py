091030521728c7392b87e344d9f296bb9c6ef310
AlexKnyshov <alexknyshov@gmail.com>
2016-02-12 11:29:47 -0800

    comments fixed

--- collapse_nodes.py
+++ collapse_nodes.py
@@ -20,43 +20,16 @@ test = tree.as_phyloxml()
 tabulate_names(test)
 d = {}
 for clade in test.find_clades():
-	#print clade, clade.name
 	if len(clade.confidences) > 0:
-		#print clade.name
-		support = clade.confidences[0].value
-		d[int(clade.name)]=support
-		#print "support", support
-		# if support < 70:
-		# 	test.collapse(clade)
-		# 	print "clade", clade.name, "collapsed", support
+		d[int(clade.name)] = clade.confidences[0].value
 count = 0
 for dx in sorted(d, reverse=True):
-#	print dx, d[dx]
 	if d[dx] < 70:
-#		print "low"
 		test.collapse(str(dx))
 		count += 1
 		print "clade", dx, "collapsed", d[dx]
 
-# for clade in test.find_clades():
-#  	print clade
-#  	if len(clade.confidences) > 0:
-#  		support = clade.confidences[0].value
-#  		print "support", support
-
-print count
+print count, "nodes collapsed"
 
 #Phylo.draw_ascii(test)
 Phylo.write(test, f+".newick.txt", "newick")
-
-# outer = 0
-# inner = 0
-# textf = open(f+".newick.txt", "r")
-# for l in textf:
-# 	for c in l:
-# 		print c
-# 		if c == "(":
-# 			outer += 1
-# 		elif c == ")":
-# 			inner += 1
-# print outer, inner
