420ce9c846dd6a509d074cd49756a2378a917531
AlexKnyshov <alexknyshov@gmail.com>
2016-02-23 08:25:35 -0800

    collapse replaced with collapse_all

--- collapse_nodes.py
+++ collapse_nodes.py
@@ -16,20 +16,10 @@ def tabulate_names(tree):
 f = sys.argv[1]
 
 tree = Phylo.read(f, "newick")
-test = tree.as_phyloxml()
-tabulate_names(test)
-d = {}
-for clade in test.find_clades():
-	if len(clade.confidences) > 0:
-		d[int(clade.name)] = clade.confidences[0].value
-count = 0
-for dx in sorted(d, reverse=True):
-	if d[dx] < 70:
-		test.collapse(str(dx))
-		count += 1
-		print "clade", dx, "collapsed", d[dx]
 
-print count, "nodes collapsed"
+tree.collapse_all(lambda c: c.confidence is not None and c.confidence < 70)
+
+
 
 #Phylo.draw_ascii(test)
-Phylo.write(test, f+".newick.txt", "newick")
+Phylo.write(tree, f+".newick", "newick")
