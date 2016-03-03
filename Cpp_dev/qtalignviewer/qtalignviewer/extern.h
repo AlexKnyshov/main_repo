e6f9a8496aa2297af79b04ea7f99b7d9f5c59164
AlexKnyshov <alexknyshov@gmail.com>
2016-03-03 11:11:50 -0800

    extern.h added

---
+++ Cpp_dev/qtalignviewer/qtalignviewer/extern.h
@@ -0,0 +1,11 @@
+#ifndef EXTERN_H
+#define EXTERN_H
+#include <string>
+#include <vector>
+
+extern int COLS;
+extern int ROWS;
+
+extern std::vector< std::vector<std::string> > vector1; //global vector storing alignment
+
+#endif // EXTERN_H
