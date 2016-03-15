6231429b1c432c8182c1584d461978d7675702d2
AlexKnyshov <alexknyshov@gmail.com>
2016-03-14 18:36:39 -0700

    essential functions added

--- taxon_resampler.py
+++ taxon_resampler.py
@@ -1,4 +1,5 @@
 from Bio import SeqIO
+from Bio import AlignIO
 import sys
 import glob
 import os
@@ -7,15 +8,32 @@ import operator
 print "init..."
 folder = sys.argv[1] #folder with mfs
 ext = sys.argv[2] #ext of mfs
-files = glob.glob(f+"/*"+ext)
+files = glob.glob(folder+"/*"+ext)
 infile = sys.argv[3] #old phylip file
+outfolder = sys.argv[4] #new folder with trimmed taxon sets
 
+ref_dict = set()
 print "trying open phylip..."
-ref_align = SeqIO.parse(infile, "phylip")
+ref_align = AlignIO.read(infile, "phylip-relaxed")
+for refseq in ref_align:
+	ref_dict.add(refseq.id)
+
+print "creating a new folder", outfolder
+if not os.path.exists (outfolder):
+    os.makedirs(outfolder) #creating folder if necessary
 
 print "parsing mfs..."
 for f in files:
+	print f.split("/")[-1]
 	input_handle = open(f, "rU")
 	alignments = SeqIO.parse(input_handle, "fasta")
+	newal = []
+	for seq in alignments:
+		if seq.id in ref_dict:
+			print seq.id, "found"
+			newal.append(seq)
+	outfile = open(outfolder+f.split("/")[-1], "w")
+	SeqIO.write(newal, outfile, "fasta")
+	outfile.close()
 
 print "done"
