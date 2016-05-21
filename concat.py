3888a8a92193b8bcf569f4ee4440a6913a2ef9df
AlexKnyshov <alexknyshov@gmail.com>
2016-05-20 17:08:11 -0700

    added pf2 cfg file support

--- concat.py
+++ concat.py
@@ -8,21 +8,22 @@ import sys
 import glob
 import os
 
-if len(sys.argv) >= 4:
+if len(sys.argv) >= 5:
 	inputfolder = sys.argv[1]
 	partnum = sys.argv[2]
 	phyliptype = sys.argv[3]
-	if len(sys.argv) == 5:
-		exclusion_file = sys.argv[4]
+	pf2opt = sys.argv[4]
+	if len(sys.argv) == 6:
+		exclusion_file = sys.argv[5]
 else:
-	print "FORMAT: python concat.py [folder with fasta] [split to codon positions: -3 (yes), -1 (no)] [phylip type: -i (interleaved), -s (sequential)] ([exclusion list])"
-	print "EXAMPLE: python concat.py ./fasta -1 -i"
-	print "EXAMPLE: python concat.py ./fasta -1 -s list.lst"
+	print "FORMAT: python concat.py [folder with fasta] [split to codon positions: -3 (yes), -1 (no)] [partition_finder output: -pf2y, -pf2n] [phylip type: -i (interleaved), -s (sequential)] ([exclusion list])"
+	print "EXAMPLE: python concat.py ./fasta -1 -i -pf2n"
+	print "EXAMPLE: python concat.py ./fasta -1 -s -pf2y list.lst"
 	print "output is written to COMBINED.phy, partitions are written to partitions.prt"
 	sys.exit()
 
 exclusion_list = []
-if len(sys.argv) == 5:
+if len(sys.argv) == 6:
 	print "reading exclusion list..."
 	exfile = open(exclusion_file, "r")
 	for line in exfile:
@@ -56,6 +57,13 @@ for dx in d.keys():
 #print align
 counter = 0
 outputfile = open("partitions.prt", "w")
+if pf2opt == "-pf2y":
+	pf2cfg = open("partition_finder.cfg", "w")
+ 	print >> pf2cfg, "alignment = COMBINED.phy;"
+ 	print >> pf2cfg, "branchlengths = linked;"
+ 	print >> pf2cfg, "models = all;"
+ 	print >> pf2cfg, "model_selection = BIC;"
+ 	print >> pf2cfg, "[data_blocks]"
 for f in files:
 	temp = MultipleSeqAlignment([], Gapped(IUPAC.ambiguous_dna)) #temp ali
  	fnew = f.split("/")
@@ -82,14 +90,25 @@ for f in files:
  	sys.stdout.flush()
  	align = temp2
  	counter += align.get_alignment_length()
+ 	if pf2opt == "-pf2y":
+ 		if partnum == "-3":
+			print >> pf2cfg, fn[:-4]+"_1 = "+str(start)+" - "+str(end)+"\\3;"
+			print >> pf2cfg, fn[:-4]+"_2 = "+str(start+1)+" - "+str(end+1)+"\\3;"
+			print >> pf2cfg, fn[:-4]+"_3 = "+str(start+2)+" - "+str(end+2)+"\\3;"
+		elif partnum == "-1":
+			print >> pf2cfg, fn[:-4]+" = "+str(start)+" - "+str(end)+"\\3;"
  	if partnum == "-3":
 		print >> outputfile, "DNA, "+fn+"-1 = "+str(start)+" - "+str(end)+"\\3"
 		print >> outputfile, "DNA, "+fn+"-2 = "+str(start+1)+" - "+str(end)+"\\3"
 		print >> outputfile, "DNA, "+fn+"-3 = "+str(start+2)+" - "+str(end)+"\\3"
 	elif partnum == "-1":
 	 	print >> outputfile, "DNA, "+fn+" = "+str(start)+" - "+str(end)
-print "\ndone"
+if pf2opt == "-pf2y":
+	print >> pf2cfg, "[schemes]"
+	print >> pf2cfg, "search = greedy;"
+	pf2cfg.close()
 outputfile.close()
+print "\ndone"
 
 print "writing..."
 if phyliptype == "-i":
