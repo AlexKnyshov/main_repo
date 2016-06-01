183aeca027b8a8e46a9cc2eb434193cbd9bb154b
AlexKnyshov <alexknyshov@gmail.com>
2016-05-31 19:43:53 -0700

    multiple output contigs can now show up in the output

--- blast/folder_blast.sh
+++ blast/folder_blast.sh
@@ -34,11 +34,12 @@ do
 	 		fi
 	 	fi
 	done < $f
- 	echo -ne $zo"% blast $f against $2...\r"
+	name=$(echo $f | rev | cut -d'/' -f-1 | rev)
+ 	echo -ne $zo"% blast $name against $2...\r"
  	CT2=$CT2+1
  	touch output.blast
  	$4 -db $2 -query fasextr.blast -out output.blast -outfmt 6 -num_threads 4 -num_alignments 1 -evalue $3
- 	cat output.blast | sort -k1,1 -k12,12nr -k11,11n | sort -u -k1,1 --merge >> blast.blast
+ 	cat output.blast | sort -k1,1 -k12,12nr -k11,11n | sort -u -k2,2 --merge >> blast.blast
 done
 #echo -ne "                                                                          \r"
 echo ""
