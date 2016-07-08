5f7b0a431fdccb9cf658b32abfc111ff6d7e27e0
AlexKnyshov <alexknyshov@gmail.com>
2016-07-08 13:29:02 -0700

    gc added

---
+++ phylo_info/gc.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import glob
+import numpy
+#import operator
+#import socket
+from Bio import AlignIO
+from Bio.SeqUtils import GC
+#filepath input
+if len(sys.argv) == 2:
+	inputfolder = sys.argv[1]
+	files = glob.glob(inputfolder+"/*")
+else:
+	print "FORMAT: python gc.py [folder with files]"
+	print "EXAMPLE: python gc.py ./folder"
+	sys.exit()
+if len(files) == 0:
+	print "no files in the directory"
+
+#starting to process files
+loci = {}
+progbarc = 0
+
+for f in files:
+	print f
+	infile = open(f, "r")
+	alignment = AlignIO.read(infile, "fasta")
+	seqlist = []
+	for seq in alignment:
+		seqlist.append(GC(seq.seq))
+	loci[f] = numpy.mean(seqlist)
+
+outf = "locigc.tab"
+with open(outf, "w") as outfile:
+	for lc, lcv in loci.items():
+		print >> outfile, lc, "\t", lcv
