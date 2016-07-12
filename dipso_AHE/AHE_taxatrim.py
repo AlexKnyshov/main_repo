848f701e6a0b5ee34c68792d27df30dc54c4e6f5
AlexKnyshov <alexknyshov@gmail.com>
2016-07-12 09:04:59 -0700

    only subsets loci

--- dipso_AHE/AHE_taxatrim.py
+++ dipso_AHE/AHE_taxatrim.py
@@ -4,38 +4,25 @@ import glob
 import os
 import shutil
 
-f = sys.argv[1]
-ext = sys.argv[2]
-files = glob.glob(f+"/*"+ext)
+if len(sys.argv) == 3:
+    f = sys.argv[1]
+    files = glob.glob(f+"/*")
+    listfile = sys.argv[2]
+else:
+    print "FORMAT: python AHE_taxatrim.py [folder with fasta] [trimlist]"
+    print "EXAMPLE: python AHE_taxatrim.py ./fasta list.lst"
+    sys.exit()
 
+translist = []
+if len(sys.argv) == 6:
+    print "reading exclusion list..."
+    lfile = open(listfile, "r")
+    for line in lfile:
+        l = line.strip()
+        translist.append(l)
+    lfile.close()
+    print "read", len(translist), "records"
 
-# d = {}
-# trimlist = {}
-# trimlist["I13432_ED_4993_Hemiptera_Dipsocoridae_Cryptostemma_sp_seq1"] = "Cryptostemma_sp_Peru_249"
-# trimlist["I13433_ED_2045_Hemiptera_Ceratocombidae_Kvamula_sp_seq1"] = "cf_Kvamula_or_Seychellesanus_sp_Madagascar_2043"
-# trimlist["I13434_ED_2660_Hemiptera_Schizopteridae_Williamsocoris_sp_seq1"] = "Williamsocoris_sp_Trinidad_2660"
-# trimlist["I13435_ED_4258_Hemiptera_Schizopteridae_Nannocoris_sp_seq1"] = "Nannocoris_sp_4258"
-# trimlist["I13436_ED_1692_Hemiptera_Schizopteridae_Kokeshia_sp_seq1"] = "Kokeshia_sp_Thailand_1409"
-# trimlist["I13437_ED_4257_Hemiptera_Schizopteridae_Chinannus_sp_seq1"] = "Chinannus_monteverdensis_4257"
-# trimlist["I13438_ED_2192_Hemiptera_Schizopteridae_Dundonannus_sp_seq1"] = "Dundonannus_sp_2190"
-# trimlist["I13439_ED_6303_Hemiptera_Schizopteridae_Schizoptera_sp_seq1"] = "Schizoptera_sp_6303"
-
-# #trimlist["I13445_RCW_4101_Hemiptera_Phymatinae_Phymata_pacifica_seq1"] = "Phymata"
-
-# print "input"
-# for infile in files:
-# 	input_handle = open(infile, "rU")
-# 	alignments = SeqIO.parse(input_handle, "fasta")
-# 	print "read", infile
-# 	outhandle = open(infile+".tx_tm.fas", "w")
-# 	for seq in alignments:
-# 		#if seq.id in trimlist:
-# 		if seq.id in trimlist.values():
-# 			print >> outhandle, ">"+trimlist[seq.id]
-# 			print >> outhandle, seq.seq
-# print "done"
-
-translist = ["EnspE", "Lican", "CespC", "Beflu", "SRR92"]
 locilist = set()
 print "input"
 for infile in files:
