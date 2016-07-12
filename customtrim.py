7f51877da7e316df496407cd591d9167f9df2754
AlexKnyshov <alexknyshov@gmail.com>
2016-07-12 14:10:57 -0700

    -d option added

--- customtrim.py
+++ customtrim.py
@@ -2,14 +2,21 @@ import os
 import sys
 import glob
 from Bio import AlignIO
+from Bio.Alphabet import IUPAC, Gapped
+from Bio.Seq import Seq 
+from Bio.SeqRecord import SeqRecord 
+from Bio.Align import MultipleSeqAlignment
 #filepath input
-if len(sys.argv) == 3:
+if len(sys.argv) >= 3:
 	inputfolder = sys.argv[1]
 	files = glob.glob(inputfolder+"/*.fas")
 	trimopt = sys.argv[2]
+	if trimopt == "-a" or trimopt == "-1":
+		exclusion_file = sys.argv[3]
 else:
-	print "FORMAT: python customtrim.py [folder with fasta] [trimming option: -a, -1, -%, -refine]"
-	print "EXAMPLE: python customtrim.py ./fasta -1"
+	print "FORMAT: python customtrim.py [folder with fasta] [trimming option: -a, -1, -%, -refine, -d] ([trimlist])"
+	print "EXAMPLE: python customtrim.py ./fasta -%"
+	print "EXAMPLE: python customtrim.py ./fasta -1 trimlist.txt"
 	sys.exit()
 if len(files) == 0:
 	print "no fasta files in the directory"
@@ -18,7 +25,16 @@ if len(files) == 0:
 print "initializing..."
 loci = {}
 #taxalist=["I13432_ED_4993_Hemiptera_Dipsocoridae_Cryptostemma_sp_seq1", "I13433_ED_2045_Hemiptera_Ceratocombidae_Kvamula_sp_seq1", "I13434_ED_2660_Hemiptera_Schizopteridae_Williamsocoris_sp_seq1", "I13435_ED_4258_Hemiptera_Schizopteridae_Nannocoris_sp_seq1", "I13436_ED_1692_Hemiptera_Schizopteridae_Kokeshia_sp_seq1", "I13437_ED_4257_Hemiptera_Schizopteridae_Chinannus_sp_seq1", "I13438_ED_2192_Hemiptera_Schizopteridae_Dundonannus_sp_seq1", "I13439_ED_6303_Hemiptera_Schizopteridae_Schizoptera_sp_seq1", "I13440_P14_RCW_1261_Hemiptera_Reduviinae_Opisthacidius_sp_seq1", "I13442_UCRC_ENT_00092725_Hemiptera_Tribelocephalinae_Afrodecius_sp_seq1", "I13443_RCW_4586_Hemiptera_Vesciinae_Vescia_sp_seq1", "I13444_RCW_4525_Hemiptera_Reduviinae_Rulandus_phaedrus_seq1", "I13445_RCW_4101_Hemiptera_Phymatinae_Phymata_pacifica_seq1"]
-taxalist=["Chinannus_monteverdensis_4257", "Schizoptera_sp_6303", "cf_Kvamula_or_Seychellesanus_sp_Madagascar_2043", "Williamsocoris_sp_Trinidad_2660", "Nannocoris_sp_4258", "Dundonannus_sp_2190", "Cryptostemma_sp_Peru_249", "Kokeshia_sp_Thailand_1409"]
+#taxalist=["Chinannus_monteverdensis_4257", "Schizoptera_sp_6303", "cf_Kvamula_or_Seychellesanus_sp_Madagascar_2043", "Williamsocoris_sp_Trinidad_2660", "Nannocoris_sp_4258", "Dundonannus_sp_2190", "Cryptostemma_sp_Peru_249", "Kokeshia_sp_Thailand_1409"]
+if sys.argv[2] == "-a" or sys.argv[2] == "-e":
+	print "reading taxalist..."
+	taxalist = []
+	exfile = open(exclusion_file, "r")
+	for line in exfile:
+		l = line.strip()
+		taxalist.append(l)
+	exfile.close()
+	print "read", len(taxalist), "records"
 warninglist = []
 progbarc = 0
 print "creating log file and output folder..."
@@ -36,12 +52,13 @@ for f in files:
 	print >> outf, "output:", fn2
 	infile = open(f, "r")
 	seqs = {}
-	for seq in AlignIO.read(infile, "fasta"):
+	inputalignment = AlignIO.read(infile, "fasta")
+	for seq in inputalignment:
 	    seqs[seq.id] = str(seq.seq).upper()
 	#pairwise
 	names =[]
 	lengths = []
-	if trimopt == "-%" or trimopt == "-refine": #trim counting all taxa
+	if trimopt == "-%" or trimopt == "-refine" or trimopt == "-d": #trim counting all taxa
 		for key in seqs.keys():
 			names.append(key)
 			lengths.append(len(seqs[key]))
@@ -99,7 +116,7 @@ for f in files:
 				break
 			#print misdata, "misdata", startpos, "startpos"
 			startpos += 1
-	if trimopt == "-refine":
+	elif trimopt == "-refine":
 		datablock = 0
 		dataflag = False
 		potential_startpos = 0
@@ -130,6 +147,20 @@ for f in files:
 			startpos += 1
 		print >> outf, "datablock (L to R): ", datablock
 		startpos = potential_startpos
+	elif trimopt == "-d":
+		positions = []
+		for basenum in range(len(seqs[names[0]])):
+			baselist = []
+			misdata = 0
+			for name in names:
+				baselist.append(seqs[name][basenum])
+			for base in baselist:
+				if base == "-" or base == "N" or base == "?":
+					misdata += 1
+				if misdata > 1:
+					break
+			if misdata <= 1:
+				positions.append(basenum)
 	#print startpos, "startpos"
 	#reverse trim
 	if trimopt == "-a":
@@ -176,7 +207,7 @@ for f in files:
 				break
 			#print misdata, "misdata", startpos, "startpos"
 			endpos -= 1
-	if trimopt == "-refine":
+	elif trimopt == "-refine":
 		datablock = 0
 		dataflag = False
 		potential_endpos = endpos
@@ -209,14 +240,28 @@ for f in files:
 		endpos = potential_endpos
 	#print endpos, "endpos"
 	infile.close()
-	print >> outf, "startpos: ", startpos
-	print >> outf, "endpos: ", endpos
+	if trimopt == "-d":
+		print >> outf, len(positions),"good positions:", positions
+	else:
+		print >> outf, "startpos: ", startpos
+		print >> outf, "endpos: ", endpos
 
 	#writing to files
-	if endpos-startpos > 0:
+	if endpos-startpos > 0 or trimopt == "-d":
 		outfile = open(fn2, "w")
-		for seq, s in seqs.items():
-			print >> outfile, ">"+seq, "\n", s[startpos:endpos]
+		if trimopt == "-d":
+			align = MultipleSeqAlignment([], Gapped(IUPAC.ambiguous_dna)) # new ali
+			for tempseq in inputalignment:
+				seqrec = SeqRecord(Seq("", Gapped(IUPAC.ambiguous_dna)), id=tempseq.id) #add taxa
+				for p in positions:
+					seqrec += tempseq[p]
+				align.extend([seqrec])
+			#AlignIO.write(align, outfile, "fasta")
+			for aliseq in align:
+				print >> outfile, ">"+aliseq.id, "\n", aliseq.seq
+		else:
+			for seq, s in seqs.items():
+				print >> outfile, ">"+seq, "\n", s[startpos:endpos]
 		outfile.close()
 	else:
 		warninglist.append(f)
@@ -228,7 +273,10 @@ for f in files:
 	#hashes = '#' * int(progbar * 0.2)
 	#spaces = ' ' * (20 - len(hashes))
 	#print "\rProgress: [{0}] {1}%".format(hashes + spaces, progbar)
-	prog = str(progbar)+"% working on file "+str(f)+": starts "+str(startpos)+", ends "+str(endpos)
+	if trimopt == "-d":
+		prog = str(progbar)+"% working on file "+str(f)+": "+str(len(positions))+" good positions"
+	else:
+		prog = str(progbar)+"% working on file "+str(f)+": starts "+str(startpos)+", ends "+str(endpos)
  	sys.stdout.write(prog+"\r")
  	sys.stdout.flush()
 print "\nwarning list:", len(warninglist)
