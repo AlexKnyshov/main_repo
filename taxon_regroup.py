633dbd6dbd50218685a71e98162392612c04daf7
AlexKnyshov <alexknyshov@gmail.com>
2016-10-14 14:39:10 -0400

    AlignIO replaced with SeqIO

--- taxon_regroup.py
+++ taxon_regroup.py
@@ -1,10 +1,8 @@
 import sys
-#import csv
 import glob
 import os
 from Bio import SeqIO
 from Bio import AlignIO
-#from Bio.Alphabet import IUPAC, Gapped
 from Bio import Phylo
 
 if len(sys.argv) >= 3:
@@ -28,140 +26,39 @@ else:
 	print "EXAMPLE: python taxon_regroup.py -seqlen ./fasta"
 	sys.exit()
 
-#csvname = sys.argv[1]
 inputfolder = sys.argv[2]
 
 files = glob.glob(inputfolder+"/*.fas")
 if not os.path.exists ("./regrouped"):
 	os.makedirs("./regrouped")
 for f in files:
-	alignment = AlignIO.read(f, "fasta")#, alphabet=Gapped(IUPAC.ambiguous_dna))
-	if sys.argv[1] == "-tree":
-		alignment.sort(key = lambda r: d[r.id])
-	elif sys.argv[1] == "-seqlen":
-		alignment.sort(key = lambda r: len(str(r.seq).replace("-", "").upper().replace("N", "")))#,  reverse=True)
-	#alignment.sort()
-	# for seq in alignment:
-	# 	print lambda r: d[seq.id]
+	fhandle = open(f, "r")
+	sortdict = {}
+	for seq in SeqIO.parse(fhandle, "fasta"):
+		sortdict[seq.id] = seq.seq
+	fhandle.close()
 	fnew = f.split("/")
 	fn = fnew[len(fnew)-1]
 	fn2 = "./regrouped/"+fn.split(".")[0]+".fas"
-	outfile = open(fn2, "w")
-	AlignIO.write(alignment, outfile, "fasta")
-	outfile.close()
+	fhandle2 = open(fn2, "w")
+	if sys.argv[1] == "-seqlen":
+		for key in sorted(sortdict, key=lambda value: len(sortdict[value])):# lambda r: len(str(value).replace("-", "").upper().replace("N", ""))):
+			print >> fhandle2, ">L"+str(key)
+			print >> fhandle2, sortdict[key]
+	if sys.argv[1] == "-tree":
+		for key in sorted(sortdict, key = lambda r: d[r.id]):# lambda r: len(str(value).replace("-", "").upper().replace("N", ""))):
+			print >> fhandle2, ">L"+str(key)
+			print >> fhandle2, sortdict[key]
+	fhandle2.close()
 
-#db = {}
-
-#Enicos = []
-#Ceratos = []
-#Dipsos = []
-#Hypsos = []
-#Ogeriinae = []
-#Schizos = []
-#Others = []
-
-#Hypsolist = ["Duonota", "Glyptocombus", "Hypselosoma", "Lativena", "Macromannus", "Pateena", "Rectilamina", "Williamsocoris", "Ommatides", "Hypselosomatinae", "NrHypselosoma"]
-#Ogerolist = ["Chinannus", "Kaimon", "Kokeshia", "Ogeria", "nr Kokeshia", "nr Ogeria", "nr Pachyplagioides"]
-#Schizolist = ["Ceratocomboides", "Corixidea", "Dundonannus", "Hoplonannus", "Membracioides", "Nannocoris", "Pinochius", "Ptenidiophyes", "Schizoptera", "Voccoroda", "Voragocoris", "nr Ceratocomboides", "nr Corixidea", "nr Dundonannus", "nr Hoplonannus", "nr Membracioides", "nr Ptenidiophyes", "nr Semanganannus", "nr Semangananus", "Nannodictyus"]
-
-# with open(csvname, "rb") as csvfile:
-# 	reader = csv.reader(csvfile)
-# 	taxonset = set()
-# 	for row in reader:
-# 		ED = int(row[0].split("_")[1])
-# 		infra = row[2]
-# 		family = row[3]
-# 		genus = row[5]
-# 		#print ED, infra	,family, genus
-# 		if infra == "Enicocephalomorpha":
-# 			db[(str(ED))] = "Enico"
-# 		elif family == "Ceratocombidae":
-# 			db[(str(ED))] = "Cerato"
-# 		elif family == "Dipsocoridae":
-# 			db[(str(ED))] = "Dipso"
-# 		elif family == "Schizopteridae":
-# 			if genus in Hypsolist:
-# 				db[(str(ED))] = "Hypso"
-# 			elif genus in Ogerolist:
-# 				db[(str(ED))] = "Ogeriinae"
-# 			elif genus in Schizolist:
-# 				db[(str(ED))] = "Schizo"
-# 			else:
-# 				db[(str(ED))] = "Other"
-# 				#taxonset.add(genus)
-# 		else:
-# 			print "do nothing"
-# # for x in sorted(taxonset):
-# # 	print x
-# input_handle = open(aligname, "rU")
-# alignments = SeqIO.parse(input_handle, "fasta")
-# index = 0
-# for seq in alignments:
-# 	EDfasta = seq.id.split("_")[-1]
-# 	genusfasta = seq.id.split("_")[0]
-# 	if EDfasta in db:
-# 		if db[EDfasta] == "Enico":
-# 			Enicos.append(index)
-
-# 		if db[EDfasta] == "Cerato":
-# 			Ceratos.append(index)
-
-# 		if db[EDfasta] == "Dipso":
-# 			Dipsos.append(index)
-
-# 		if db[EDfasta] == "Hypso":
-# 			Hypsos.append(index)
-
-# 		if db[EDfasta] == "Ogeriinae":
-# 			Ogeriinae.append(index)
-
-# 		if db[EDfasta] == "Schizo":
-# 			Schizos.append(index)
-
-# 		if db[EDfasta] == "Other":
-# 			if genusfasta in Hypsolist:
-# 				Hypsos.append(index)
-# 			elif genusfasta in Ogerolist:
-# 				Ogeriinae.append(index)
-# 			elif genusfasta in Schizolist:
-# 				Schizos.append(index)
-# 			else:
-# 				Others.append(index)
-# 	else:
-# 		print seq.id
-# 		Others.append(index)
-# 	index += 1
-# taxa = []
-# taxa.append(Enicos)
-# taxa.append(Ceratos)
-# taxa.append(Dipsos)
-# taxa.append(Hypsos)
-# taxa.append(Ogeriinae)
-# taxa.append(Schizos)
-# taxa.append(Others)
-# #Dipsos = []
-# #Hypsos = []
-# #Ogeriinae = []
-# #Schizos = []
-# #Others = []
-# #print taxa
-# input_handle.seek(0)
-# alignments = AlignIO.read(input_handle, "fasta")
-# #print alignments
-# outfile = open(aligname+".txt", "w")
-# group = 1
-# for g in taxa:
-# 	Goutfile = open(aligname+str(group)+".fas", "w")
-# 	for t in g:
-# 		print >> outfile, ">"+alignments[t].id
-# 		print >> outfile, alignments[t].seq
-# 		print >> Goutfile, ">"+alignments[t].id
-# 		print >> Goutfile, alignments[t].seq 
-# 	Goutfile.close()
-# 	group += 1
-# 	# if seq.id in d:
-# 	# 	d[seq.id].append(infile.split("/")[-1])
-# 	# else:
-# 	# 	d[seq.id] = []
-# 	# 	d[seq.id].append(infile.split("/")[-1])
-# outfile.close()
+	# alignment = AlignIO.read(f, "fasta")
+	# if sys.argv[1] == "-tree":
+	# 	alignment.sort(key = lambda r: d[r.id])
+	# elif sys.argv[1] == "-seqlen":
+	# 	alignment.sort(key = lambda r: len(str(r.seq).replace("-", "").upper().replace("N", "")))
+	# fnew = f.split("/")
+	# fn = fnew[len(fnew)-1]
+	# fn2 = "./regrouped/"+fn.split(".")[0]+".fas"
+	# outfile = open(fn2, "w")
+	# AlignIO.write(alignment, outfile, "fasta")
+	# outfile.close()
