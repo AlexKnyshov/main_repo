7e742911f6d0ea73db4a8682de7bd35e18e2a3a5
AlexKnyshov <alexknyshov@gmail.com>
2017-05-19 10:45:34 -0700

    seeks for *.fa* files now

--- trans-rename.sh
+++ trans-rename.sh
@@ -1,1 +1,1 @@
-for f in *.fas; do echo $f; awk -F'|' '{if ($1 ~ ">") {print ">"$2} else {print $1}}' $f > $f.fas; done
+for f in *.fa*; do echo $f; awk -F'|' '{if ($1 ~ ">") {print ">"$2} else {print $1}}' $f > $f.fas; done
