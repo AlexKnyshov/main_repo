b518bad98d611a3c1db6813a2bff2ae015287ebb
AlexKnyshov <alexknyshov@gmail.com>
2017-05-20 12:20:23 -0700

    split_phy.py added

---
+++ split_phy.py
@@ -0,0 +1,37 @@
+from Bio import AlignIO
+from Bio.Alphabet import IUPAC, Gapped
+from Bio.Seq import Seq 
+from Bio.SeqRecord import SeqRecord 
+from Bio.Align import MultipleSeqAlignment
+import csv
+import sys
+import glob
+import os
+
+concat_name = sys.argv[1]
+partname = sys.argv[2]
+
+######
+concat_handle = open(concat_name, "rU")
+partition_handle = open(partname, "r")
+alignment = AlignIO.read(concat_handle, "phylip-relaxed", alphabet = Gapped(IUPAC.protein, '-'))
+for line in partition_handle:
+	l = line.strip().split("=")
+	fname = l[0].split(",")[-1].strip()#, l[1].split(",")
+	#print fname
+	#outfile = open(fname+".fas", "w")
+	i = 1
+	for p in l[1].split(","):
+		start = int(p.split("-")[0])-1
+		end = int(p.split("-")[1])
+		outfile = open(fname+"-"+str(i)+".fas", "w")
+		for record in alignment:
+			#print start, end
+			#print record.id, record.seq[start:end]
+			print >> outfile, ">"+record.id, "\n",record.seq[start:end]
+		#outfile
+		i += 1
+
+
+concat_handle.close()
+partition_handle.close()
