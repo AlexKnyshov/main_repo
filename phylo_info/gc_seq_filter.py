3e169f4249f9081f7ebef2b7b2555adc3553a8dd
AlexKnyshov <alexknyshov@gmail.com>
2019-04-19 10:46:52 -0700

    gc_seq_filter added

---
+++ phylo_info/gc_seq_filter.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import glob
+import numpy
+import shutil
+from Bio import AlignIO
+from Bio.SeqUtils import GC
+#filepath input
+if len(sys.argv) == 2:
+	inputfolder = sys.argv[1]
+	files = glob.glob(inputfolder+"/*")
+else:
+	print "FORMAT: python gc_seq_filter.py [folder with files]"
+	print "EXAMPLE: python gc_seq_filter.py ./folder"
+	sys.exit()
+if len(files) == 0:
+	print "no files in the directory"
+
+print "creating an output folder..."
+if not os.path.exists ("./gc_reduced/"):
+    os.makedirs("./gc_reduced") #creating folder if necessary
+else:
+    shutil.rmtree("./gc_reduced/") #removing old files
+    os.makedirs("./gc_reduced")
+
+#starting to process files
+outf = open("gc_out.tab", "w")
+for f in files:
+	fl = f.split("/")[-1]
+	infile = open(f, "r")
+	alignment = AlignIO.read(infile, "fasta")
+	allen = float(alignment.get_alignment_length())
+	seqlist = {}
+	for seq in alignment:
+		if len(str(seq.seq).replace("-","").replace("N","")) / allen > 0.25:
+			seqlist[seq.id] = GC(seq.seq)
+	avg = numpy.mean(seqlist.values())
+	outliers = []
+	for k, v in seqlist.items():
+		if abs(v-avg) > 20:
+			print >> outf, "locus", fl, "outlier", k, "gc", v, "avg", avg
+			# print "avg", avg
+			outliers.append(k)
+	aliout = open("./gc_reduced/"+fl, "w")
+	for seq in alignment:
+		if seq.id not in outliers:
+			print >> aliout, ">"+seq.id, "\n", seq.seq
+	# aliin = open(f, "rU")
+	# for seq in SeqIO.parse(aliin, "fasta"):
+	# 	if seq.id not in outliers:
+	# 	 	print >> aliout, ">"+seq.id, "\n", seq.seq
+ # 	status = "long branches detected"
+	aliout.close()
+	# aliin.close()
+	infile.close()
+outf.close()
+print "done"
