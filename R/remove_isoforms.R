d91429ff106060c621b4a885467bcb382cf7bf5f
AlexKnyshov <alexknyshov@gmail.com>
2019-06-13 16:09:21 -0700

    fix11

--- R/remove_isoforms.R
+++ R/remove_isoforms.R
@@ -14,13 +14,13 @@ for (f in files){
 	colnames(mx) <- trimws(colnames(mx))
 	diag(mx) <- NA
   loop_OK <- TRUE
+  cout1 <- character(length = 0)
   while (loop_OK == TRUE){
 	#mx[lower.tri(mx, diag = T)] <- NA
+	loop_OK <- FALSE
 	mx2 <- which(mx < 0.01, arr.ind = TRUE)
 	if (length(mx2) > 0){
-	  cout1 <- character(length = 0)
 	  #print(unique(rownames(mx2)))
-	  loop_OK <- FALSE
 	  for (r in 1:nrow(mx2)){
 	  	name1 <- unlist(strsplit(rownames(mx)[mx2[r,1]], "\\|"))
 	  	name2 <- unlist(strsplit(rownames(mx)[mx2[r,2]], "\\|"))
@@ -29,13 +29,17 @@ for (f in files){
 	  		len1 <- length(locus_text[rownames(mx)[mx2[r,1]],][locus_text[rownames(mx)[mx2[r,1]],]!="-"])
 	  		len2 <- length(locus_text[rownames(mx)[mx2[r,2]],][locus_text[rownames(mx)[mx2[r,2]],]!="-"])
 	  		if (len1 >= len2){
+	  			# print(paste0("good:_",rownames(mx)[mx2[r,1]]))
 	  			print(paste0("excluded:_",rownames(mx)[mx2[r,2]]))
+	  			cout1 <- c(cout1,(rownames(mx)[mx2[r,2]]))
 	  			mx <- mx[-mx2[r,2],-mx2[r,2]]
 	  			loop_OK <- TRUE
 	  			break
 	  		}
 	  		else {
+	  			# print(paste0("good:_",rownames(mx)[mx2[r,2]]))
 				print(paste0("excluded:_",rownames(mx)[mx2[r,1]]))
+				cout1 <- c(cout1,(rownames(mx)[mx2[r,1]]))
 				mx <- mx[-mx2[r,1],-mx2[r,1]]
 				loop_OK <- TRUE
 	  			break
@@ -61,20 +65,23 @@ for (f in files){
 	    #   cout1 <- c(cout1,(rownames(mx)[mx2[r,1]]))
 	    # }
 	  }
-	  # if (length(cout1)>0){
-	  #   write(f, "out.tab", append = T)
-	  #   write(unique(cout1), "out.tab", append = T)
-	  #   drop1 <- match(unique(cout1),rownames(mx))
-	  #   locus <- locus[-drop1,]
-	  #   # for (n in unique(cout1)){
-	  #   #   print(typeof(locus))
-	  #   #   print(which(rownames(mx)==n))
-	  #   #   locus[which(rownames(mx)==n),, drop=T]
-	  #   # }
-	  #   #
-	  # }
-	  # f1 <- paste("./ccout/", unlist(strsplit(f, "//"))[2], sep='')
-	  # write.FASTA(locus, f1)
+
 	}
 }
+	  if (length(cout1)>0){
+	    write(f, "out.tab", append = T)
+	    write(unique(cout1), "out.tab", append = T)
+	    drop1 <- match(cout1,rownames(locus_text))
+	    #print(cout1)
+	    locus <- locus[-drop1,]
+	    # for (n in unique(cout1)){
+	    #   print(typeof(locus))
+	    #   print(which(rownames(mx)==n))
+	    #   locus[which(rownames(mx)==n),, drop=T]
+	    # }
+	    #
+	  }
+	  dir.create("no_iso")
+	  f1 <- paste("./no_iso/", unlist(strsplit(f, "//"))[2], sep='')
+	  write.FASTA(locus, f1)
 }
