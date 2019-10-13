63d273b4db5d17d50cadea2616548a9f91586388
AlexKnyshov <alexknyshov@gmail.com>
2019-10-13 13:04:32 -0700

    reformat_UCE_probes_phyluce.py

---
+++ reformat_UCE_probes_phyluce.py
@@ -0,0 +1,19 @@
+from Bio import SeqIO
+import sys
+
+
+if len(sys.argv) >= 3:
+	infname = sys.argv[1]
+
+else:
+	print "FORMAT: argument1 = probe file in fasta format"
+	print "EXAMPLE: ./fasta.fas"
+	print "output is written to [fasta.fas]_mod.fas"
+	sys.exit()
+
+with open(infname) as infhandle:
+	with open(infname+"_mod.fas", "w") as outh:
+		seqs = SeqIO.parse(infhandle, "fasta")
+		for seq in seqs:
+			print >> outh, ">"+seq.id+"_p1"
+			print >> outh, seq.seq
