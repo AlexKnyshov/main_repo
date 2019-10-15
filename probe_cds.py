495182882f66a6fefebd77536343a141ec354943
AlexKnyshov <alexknyshov@gmail.com>
2019-10-14 19:10:06 -0700

    fix2

--- probe_cds.py
+++ probe_cds.py
@@ -49,47 +49,82 @@ def getOverlap(a, b):
 	b1=max(b)
 	return max(0, min(a1, b1) - max(a0, b0))
 
-def determine_direction(a,b,c,d):
-	if a < b and c < d:
+def determine_direction(a,b):
+	if a < b:
 		return True
-	elif a < b and c > d:
+	else:
 		return False
-	elif a > b and c < d:
-		return False
-	elif a > b and c > d:
-		return True
 
 gffout = parse_gff(gfffile)
 
 mkdirfunc("CDS")
 
+badlist = {}
 with open(blastfile,"rU") as blasthandle:
 	reader = csv.reader(blasthandle, delimiter='\t')
 	for row in reader:
+		print row[0]
 		contig = row[1]
 		if contig in gffout:
+			ovlp = False
 			for cds in gffout[contig]:
 				if getOverlap(cds[0:2],[int(row[8]), int(row[9])]) > 0:
-					hitdirection = determine_direction(int(row[6]),int(row[7]),int(row[8]),int(row[9]))
+					ovlp = True
 					targets = min(int(row[8]), int(row[9]))
 					targete = max(int(row[8]), int(row[9]))
+					targetdirection = determine_direction(int(row[8]),int(row[9]))
 					querys = min(int(row[6]), int(row[7]))-1
 					querye = max(int(row[6]), int(row[7]))-1
+					querydirection = determine_direction(int(row[6]),int(row[7]))
 					if cds[0] < targets:
-						startgap = 0
-					else:
-						startgap = cds[0] - targets
-					if cds[1] > targete:
-						endgap = 0
-					else:
-						endgap = targete - cds[1]
-					with open(row[0]) as infhandle:
-						seq = SeqIO.read(infhandle, "fasta")
-						if hitdirection:
-							outseq = seq[(querys+startgap):(querye-endgap)].translate()
+						if querydirection == targetdirection:
+							startgap = 0
 						else:
-							outseq = seq[(querys+endgap):(querye-startgap)].reverse_complement().translate()
+							endgap = 0
+					else:
+						if querydirection == targetdirection:
+							startgap = cds[0] - targets
+						else:
+							endgap = cds[0] - targets
+					if cds[1] > targete:
+						if querydirection == targetdirection:
+							endgap = 0
+						else:
+							startgap = 0
+					else:
+						if querydirection == targetdirection:
+							endgap = targete - cds[1]
+						else:
+							startgap = targete - cds[1]
+					cdsdirection = cds[2]
+					# print cds, row[6:10]
+					# print "ranges", querys, querye, targets, targete
+					# print "startgap on target", startgap, "end", endgap
+					# print "directions:", querydirection, targetdirection, cdsdirection
+					with open(row[0]) as infhandle:
+						seqs = list(SeqIO.parse(infhandle, "fasta"))
+						seq = seqs[0].seq
+						# print seq[(querys+startgap):(querye-endgap)].translate()
+						if cdsdirection:
+							if querydirection == targetdirection:
+								outseq = seq[(querys+startgap):(querye-endgap)]
+							else:
+								outseq = seq[(querys+startgap):(querye-endgap)].reverse_complement()
+						else:
+							if querydirection == targetdirection:
+								outseq = seq[(querys+startgap):(querye-endgap)].reverse_complement()
+							else:
+								outseq = seq[(querys+startgap):(querye-endgap)]
 						basename = row[0].split("/")[-1]
+						# if startgap > 0 or endgap > 0:
+						# 	print basename, outseq, querys, querye
 						with open("./CDS/"+basename, "w") as outh:
-							print >> outh, ">"+outseq.id
-							print >> outh, outseq.seq
+							print >> outh, ">"+seqs[0].id
+							print >> outh, outseq
+					break
+			if not ovlp:
+				badlist[row[0]] = "region not in CDS"
+		else:
+			badlist[row[0]] = "contig not in GFF"
+	for key, val in badlist.items():
+		print key, ":", val
