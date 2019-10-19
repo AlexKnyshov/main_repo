4882b05716530ffdc50992214f41392070a4ad58
AlexKnyshov <alexknyshov@gmail.com>
2019-10-19 14:57:05 -0700

    average

--- pairwise_dist_calc.py
+++ pairwise_dist_calc.py
@@ -4,6 +4,10 @@ import glob
 
 inputfolder = sys.argv[1]
 files = glob.glob(inputfolder+"/*.fas")
+list_pc_match = []
+list_pc_mismatch = []
+list_pc_bait_gap = []
+list_pc_target_gap = []
 for f in files:
 	aln = AlignIO.read(open(f), "fasta")
 	for seq in aln:
@@ -24,7 +28,13 @@ for f in files:
 			else:
 				mismatch += 1
 	pc_match = str(round(match / float(length) * 100, 3))
+	list_pc_match.append(pc_match)
 	pc_mismatch = str(round(mismatch / float(length) * 100, 3))
+	list_pc_mismatch.append(pc_mismatch)
 	pc_bait_gap = str(round(bait_gap / float(length) * 100, 3))
+	list_pc_bait_gap.append(pc_bait_gap)
 	pc_target_gap = str(round(target_gap / float(length) * 100, 3))
+	list_pc_target_gap.append(pc_target_gap)
 	print f+'\t'+pc_match+'\t'+pc_mismatch+'\t'+pc_bait_gap+'\t'+pc_target_gap
+
+print "mean"+'\t'+sum(list_pc_match)/len(list_pc_match)+'\t'+sum(list_pc_mismatch)/len(list_pc_match)+'\t'+sum(list_pc_bait_gap)/len(list_pc_match)+'\t'+sum(list_pc_target_gap)/len(list_pc_match)
