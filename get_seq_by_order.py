4e771d158ab78767b1940569fd7ba6ba40072377
AlexKnyshov <alexknyshov@gmail.com>
2019-10-29 10:52:05 -0700

    file added

---
+++ get_seq_by_order.py
@@ -0,0 +1,16 @@
+from Bio import SeqIO
+import sys
+
+infile = sys.argv[1]
+inrange = int(sys.argv[2])
+
+with open(infile) as inhandle:
+	with open(infile+".edited", "w") as outhandle:
+		seqs = SeqIO.parse(outhandle, "fasta")
+		counter = 1
+		for seq in seqs:
+			if counter == inrange:
+				print >> outhandle, ">"+seq.id
+				print >> outhandle, seq.seq
+				break
+			counter += 1
