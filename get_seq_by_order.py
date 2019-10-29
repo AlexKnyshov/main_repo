8419efa8eee6429611e15e2ce1d1b588f7468fa7
AlexKnyshov <alexknyshov@gmail.com>
2019-10-29 13:32:25 -0700

    fix2

--- get_seq_by_order.py
+++ get_seq_by_order.py
@@ -5,12 +5,13 @@ infile = sys.argv[1]
 inrange = int(sys.argv[2])
 
 with open(infile) as inhandle:
-	with open(infile+".edited", "w") as outhandle:
-		seqs = SeqIO.parse(inhandle, "fasta")
-		counter = 1
-		for seq in seqs:
-			if counter == inrange:
-				print >> outhandle, ">"+seq.id
-				print >> outhandle, seq.seq
-				break
-			counter += 1
+	seqs = SeqIO.parse(inhandle, "fasta")
+	counter = 1
+	for seq in seqs:
+		if counter == inrange:
+			outhandle = open(infile+".edited", "w")
+			print >> outhandle, ">"+seq.id
+			print >> outhandle, seq.seq
+			outhandle.close()
+			break
+		counter += 1
