97f4ebe04175df52d306f7c3b41846cdeaacc8b5
AlexKnyshov <alexknyshov@gmail.com>
2020-05-07 16:17:00 -0700

    phangorn, efficacy of search, sort output

--- R/rfFilter.R
+++ R/rfFilter.R
@@ -1,4 +1,5 @@
 library(ape)
+library(phangorn)
 
 args = commandArgs(trailingOnly=TRUE)
 if (length(args) == 0){
@@ -26,24 +27,25 @@ FindOutliers <- function(data) {
 if (length(args) == 3){
 	tree_dist <- data.frame(locus=character(), rf = numeric())
 	for (t in 1:length(gtrees)){
-	  print (names(gtrees[t]))
 	  temptree <- drop.tip(tree, tree$tip.label[!(tree$tip.label %in% gtrees[[t]]$tip.label)])
-	  
-	  distval = as.numeric(dist.topo(gtrees[t], temptree))
-	  print (distval)
+	  distval <- wRF.dist(unroot(gtrees[t]), unroot(temptree))
 	  tree_dist <- rbind(tree_dist, data.frame(locus=names(gtrees[t]), rf=distval))
 	}
 } else {
-	tree_dist <- data.frame(locus=character(), rf = numeric())
-	for (t in 1:length(gtrees)){
-	  distval <- numeric()
-	  for (t2 in 1:length(gtrees)){
-	    temptree1 <- drop.tip(gtrees[[t]], setdiff(gtrees[[t]]$tip.label, gtrees[[t2]]$tip.label))
-	    temptree2 <- drop.tip(gtrees[[t2]], setdiff(gtrees[[t2]]$tip.label, gtrees[[t]]$tip.label))
-	    distval <- c(distval, as.numeric(dist.topo(temptree1, temptree2)))
+	tree_dist_mx <- matrix(NA, nrow =length(gtrees), ncol=length(gtrees), dimnames = list(names(gtrees),names(gtrees)) )
+	logicmx <- lower.tri(tree_dist_mx,diag = FALSE) 
+	for (row1 in 1:length(tree_dist_mx[,1])){
+	  for (col1 in 1:length(tree_dist_mx[1,])){
+	    if (logicmx[row1, col1] == TRUE ){
+	      temptree1 <- drop.tip(gtrees[[row1]], setdiff(gtrees[[row1]]$tip.label, gtrees[[col1]]$tip.label))
+	      temptree2 <- drop.tip(gtrees[[col1]], setdiff(gtrees[[col1]]$tip.label, gtrees[[row1]]$tip.label))
+	      tree_dist_mx[row1, col1] <- wRF.dist(unroot(temptree1), unroot(temptree2))
+	    }
 	  }
-	  tree_dist <- rbind(tree_dist, data.frame(locus=names(gtrees[t]), rf=mean(distval))) 
 	}
-}
+	tree_dist_mx[upper.tri(tree_dist_mx, diag = F)] <- t(tree_dist_mx)[upper.tri(tree_dist_mx, diag=F)] 
+	tree_dist <- data.frame(locus=rownames(tree_dist_mx), rf = apply(tree_dist_mx, 1, mean, na.rm = T))
 
+}
+tree_dist <- tree_dist[order(-tree_dist$rf),]
 write(as.character(tree_dist$locus[FindOutliers(tree_dist$rf)]), "rf_loci.txt")
