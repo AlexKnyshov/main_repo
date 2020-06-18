25e747cdf4c7393e4ee4764178ab7cd0c984c6ec
AlexKnyshov <alexknyshov@gmail.com>
2020-06-18 10:33:24 -0700

    WAG model of AA dist

--- R/distfilter.R
+++ R/distfilter.R
@@ -3,7 +3,7 @@ library(seqinr)
 args = commandArgs(trailingOnly=TRUE)
 argsLen <- length(args);
 if (argsLen == 0){
-  cat("Syntax: Rscript distfilter.R [path to concat dna] [path to dna folder] [path to concat protein] [path to protein folder] ([upperN] ([lowerN])) \n")
+  cat("Syntax: Rscript distfilter.R [path to concat dna] [path to dna folder] [path to concat protein] [path to protein folder] ([upperN] ([lowerN] ([rescale]))) \n")
   cat("Example: Rscript distfilter.R concatdna.fas ./dna/ concataa.fas ./aa/\n")
   cat("Example: Rscript distfilter.R concatdna.fas ./dna/ concataa.fas ./aa/ 1.5\n")
   cat("Example: Rscript distfilter.R concatdna.fas ./dna/ concataa.fas ./aa/ 3 3\n")
@@ -12,24 +12,50 @@ if (argsLen == 0){
 if (argsLen == 4) {
 	upperN = 3
 	lowerN = 3
+	rescale = FALSE
 }
 if (argsLen == 5) {
 	upperN = as.numeric(args[5])
 	lowerN = 3
+	rescale = FALSE
 }
 if (argsLen == 6) {
 	upperN = as.numeric(args[5])
 	lowerN = as.numeric(args[6])
+	rescale = FALSE
+}
+if (argsLen == 7) {
+	upperN = as.numeric(args[5])
+	lowerN = as.numeric(args[6])
+	rescale = TRUE
+	library(phangorn)
 }
 FindOutliers <- function(data) {
   lowerq = quantile(data, na.rm = T)[2]
   upperq = quantile(data, na.rm = T)[4]
   iqr = upperq - lowerq #Or use IQR(data)
-  # we identify extreme outliers
+  # identify the outliers
   extreme.threshold.upper = (iqr * upperN) + upperq
   extreme.threshold.lower = lowerq - (iqr * lowerN)
   result <- which(data > extreme.threshold.upper | data < extreme.threshold.lower)
 }
+ComputeDistMatrix <- function(alignment) {
+	alignmentmx <- as.phyDat(alignment)
+	dist_mx_dims <- c(length(alignmentmx), length(alignmentmx[[1]]))
+	dist_mx <- matrix(NA, nrow =dist_mx_dims[1], ncol=dist_mx_dims[1], dimnames = list(names(alignmentmx),names(alignmentmx)) )
+	logicmx <- lower.tri(dist_mx,diag = FALSE) 
+	for (row1 in 1:length(dist_mx[,1])){
+	  for (col1 in 1:length(dist_mx[1,])){
+	    if (logicmx[row1, col1] == TRUE ){
+	      dist_mx[row1, col1] <- dist.ml(alignmentmx[c(row1,col1), ], model="WAG")
+	    }
+	  }
+	}
+	dist_mx[upper.tri(dist_mx, diag = F)] <- t(dist_mx)[upper.tri(dist_mx, diag=F)]
+	diag(dist_mx) <- 0
+	return (dist_mx) 
+}
+
 concatdna <- args[1]
 dnaseqpath <- args[2]
 concataa <- args[3]
@@ -47,11 +73,17 @@ proteinfiles <- list.files(path=aaseqpath, pattern="\\.fas$", full.names=FALSE, 
 commonfiles <- intersect(dnafiles, proteinfiles)
 print(paste0("files to process: ", length(commonfiles)))
 print("compute average taxon distance")
-dnadist <- dist.dna(dnaconcat, as.matrix = T, pairwise.deletion = T)
+if (rescale == TRUE) {
+	# dnadist <- ComputeDistMatrix(dnaconcat)
+	dnadist <- dist.dna(dnaconcat, as.matrix = T, pairwise.deletion = T)
+	proteindist <- ComputeDistMatrix(proteinconcat)
+} else {
+	dnadist <- dist.dna(dnaconcat, as.matrix = T, pairwise.deletion = T)
+	proteindist <- dist.aa(proteinconcat, pairwise.deletion = T)
+	proteindist <- as.matrix(proteindist)
+	proteindist <- proteindist / length(proteinconcat[1,])
+}
 avgdnadist <- apply(dnadist, 1, mean)
-proteindist <- dist.aa(proteinconcat, pairwise.deletion = T)
-proteindist <- as.matrix(proteindist)
-proteindist <- proteindist / length(proteinconcat[1,])
 avgproteindist <- apply(proteindist, 1, mean)
 print("process files")
 for (i in 1:length(commonfiles)){
@@ -67,10 +99,16 @@ for (i in 1:length(commonfiles)){
 	proteinlocus <- as.AAbin.character(proteinlocus)
 	if (length(dnalocus[,1]) > 1 & length(proteinlocus[,1]) > 1){
 		#get distance
-		dnalocusdist <- dist.dna(dnalocus, as.matrix = T, pairwise.deletion = T)
-		proteinlocusdist <- dist.aa(proteinlocus, pairwise.deletion = T)
-		proteinlocusdist <- as.matrix(proteinlocusdist)
-		proteinlocusdist <- proteinlocusdist / length(proteinlocus[1,])
+		if (rescale == TRUE) {
+			# dnalocusdist <- ComputeDistMatrix(dnalocus)
+			dnalocusdist <- dist.dna(dnalocus, as.matrix = T, pairwise.deletion = T)
+			proteinlocusdist <- ComputeDistMatrix(proteinlocus)
+		} else {
+			dnalocusdist <- dist.dna(dnalocus, as.matrix = T, pairwise.deletion = T)
+			proteinlocusdist <- dist.aa(proteinlocus, pairwise.deletion = T)
+			proteinlocusdist <- as.matrix(proteinlocusdist)
+			proteinlocusdist <- proteinlocusdist / length(proteinlocus[1,])
+		}
 		#get average distance, correct order of all matrices
 		avgdnalocusdist <- apply(dnalocusdist, 1, mean)
 		nameorder <- names(avgdnalocusdist)
