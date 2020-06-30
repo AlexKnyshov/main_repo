0a93911501c301749663c8a7aa7d92897b46d9a5
AlexKnyshov <alexknyshov@gmail.com>
2020-06-30 16:07:16 -0700

    output mask in flank modes fix2

--- customtrim.py
+++ customtrim.py
@@ -334,7 +334,7 @@ for f in files:
 			outmask = open(fn2+"_masked", "w")
 			for tempseq in inputalignment:
 				print >> outfile, ">"+tempseq.id, "\n", tempseq.seq[startpos:endpos]
-				print >> outfile, ">"+tempseq.id, "\n", ("$"*startpos)+tempseq.seq[startpos:endpos]+("$"*(len(tempseq.seq)-endpos-1))
+				print >> outmask, ">"+tempseq.id, "\n", ("$"*startpos)+tempseq.seq[startpos:endpos]+("$"*(len(tempseq.seq)-endpos-1))
 			# for seq, s in seqs.items():
 			# 	print >> outfile, ">"+seq, "\n", s[startpos:endpos]
 			outfile.close()
