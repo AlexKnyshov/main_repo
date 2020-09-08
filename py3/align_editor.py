88d50f8e629a0e014e5a43e6feecc458d5d758f4
AlexKnyshov <alexknyshov@gmail.com>
2020-09-08 12:06:55 -0700

    py3 mods

--- py3/align_editor.py
+++ py3/align_editor.py
@@ -26,23 +26,23 @@ elif tp == "Prot":
 
 alignment = AlignIO.read(infile, fmt, alphabet=alph)
 
-rms = raw_input("Enter start")
-rme = raw_input("Enter end")
+rms = input("Enter start")
+rme = input("Enter end")
 crop_start = int(rms)
 crop_end = int(rme)
 if rev:
 	alignment = alignment[:, crop_start:crop_end]
 else:
 	alignment = alignment[:, :crop_start]+alignment[:, crop_end:]
-	q = raw_input("More?")
+	q = input("More?")
 	if q == "y" or q == "Y":
 		while q == "y" or q == "Y":
-			rms = raw_input("Enter start")
-			rme = raw_input("Enter end")
+			rms = input("Enter start")
+			rme = input("Enter end")
 			crop_start = int(rms)
 			crop_end = int(rme)
 			alignment = alignment[:, :crop_start]+alignment[:, crop_end:]
-			q = raw_input("More?")
+			q = input("More?")
 
 AlignIO.write(alignment, infile+".edited", fmt)
 
