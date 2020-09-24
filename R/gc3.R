889e66cec8c7cd766c103d3162ede3bee99964dd
AlexKnyshov <alexknyshov@gmail.com>
2020-09-24 13:38:44 -0700

    R gc3 script added

---
+++ R/gc3.R
@@ -0,0 +1,62 @@
+library(ape)
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) == 0){
+  cat("Syntax: Rscript gc3.R [path to the folder with alignments]\n")
+  cat("Example: Rscript gc3.R ./fasta/\n")
+  quit()
+} else if (length(args) == 1){
+	dnaseqpath <- args[1]
+}
+
+getGCcontent <- function(data, dimension){
+	GCcontent <- apply(data, dimension, function (x) {
+		gc <- x[x == "g" | x == "c"]
+		at <- x[x == "a" | x == "t"]
+		return(length(gc) / (length(gc) + length(at)))
+		})
+	return(GCcontent)
+}
+
+dnafiles <- list.files(path=dnaseqpath, pattern="\\.fas$", full.names=FALSE, recursive=FALSE)
+locidata <- data.frame()
+for (i in 1:length(dnafiles)){
+	#path
+	dnafile <- paste0(dnaseqpath,"/",dnafiles[i])
+	dnalocus <- read.dna(dnafile, format="fasta", as.character=T, as.matrix=T)
+	# pos1
+	mask = c(T,F,F)
+	#seq based
+	seqbasedGC1 <- getGCcontent(dnalocus[,mask], 1)
+	#pos based
+	posbasedGC1 <- getGCcontent(dnalocus[,mask], 2)
+	mask = c(F,T,F)
+	#seq based
+	seqbasedGC2 <- getGCcontent(dnalocus[,mask], 1)
+	#pos based
+	posbasedGC2 <- getGCcontent(dnalocus[,mask], 2)
+	mask = c(F,F,T)
+	#seq based
+	seqbasedGC3 <- getGCcontent(dnalocus[,mask], 1)
+	#pos based
+	posbasedGC3 <- getGCcontent(dnalocus[,mask], 2)
+
+	locidata <- rbind(locidata, data.frame(locus=dnafiles[i],
+		loclen=length(dnalocus[1,]),
+		taxnum=length(dnalocus[,1]),
+		seqgc1mean=mean(seqbasedGC1, na.rm=T),
+		seqgc1sd=sd(seqbasedGC1, na.rm=T),
+		posgc1mean=mean(posbasedGC1, na.rm=T),
+		posgc1sd=sd(posbasedGC1, na.rm=T),
+		seqgc2mean=mean(seqbasedGC2, na.rm=T),
+		seqgc2sd=sd(seqbasedGC2, na.rm=T),
+		posgc2mean=mean(posbasedGC2, na.rm=T),
+		posgc2sd=sd(posbasedGC2, na.rm=T),
+		seqgc3mean=mean(seqbasedGC3, na.rm=T),
+		seqgc3sd=sd(seqbasedGC3, na.rm=T),
+		posgc3mean=mean(posbasedGC3, na.rm=T),
+		posgc3sd=sd(posbasedGC3, na.rm=T)	
+	))
+	print (paste0(dnafiles[i], " processed"))
+}
+write.csv(locidata, "gc3.csv")
