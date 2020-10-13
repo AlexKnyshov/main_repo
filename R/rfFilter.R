8dd9530fea35430d1c0aee5708a73dbd648c8b5f
AlexKnyshov <alexknyshov@gmail.com>
2020-10-13 10:07:36 -0700

    check tree sizes

--- R/rfFilter.R
+++ R/rfFilter.R
@@ -39,13 +39,17 @@ if (length(args) == 3){
 	    if (logicmx[row1, col1] == TRUE ){
 	      temptree1 <- drop.tip(gtrees[[row1]], setdiff(gtrees[[row1]]$tip.label, gtrees[[col1]]$tip.label))
 	      temptree2 <- drop.tip(gtrees[[col1]], setdiff(gtrees[[col1]]$tip.label, gtrees[[row1]]$tip.label))
-	      tree_dist_mx[row1, col1] <- wRF.dist(unroot(temptree1), unroot(temptree2))
+	      if (!is.null(temptree1) & !is.null(temptree2)) {
+	        if (length(temptree1$tip.label) > 3 & length(temptree2$tip.label) > 3){
+	          tree_dist_mx[row1, col1] <- wRF.dist(unroot(temptree1), unroot(temptree2))  
+	        }
+	      }
 	    }
 	  }
 	}
 	tree_dist_mx[upper.tri(tree_dist_mx, diag = F)] <- t(tree_dist_mx)[upper.tri(tree_dist_mx, diag=F)] 
 	tree_dist <- data.frame(locus=rownames(tree_dist_mx), rf = apply(tree_dist_mx, 1, mean, na.rm = T))
-
+	write.csv(tree_dist_mx, "rf_dist_mx.csv")
 }
 tree_dist <- tree_dist[order(-tree_dist$rf),]
 write(as.character(tree_dist$locus[FindOutliers(tree_dist$rf)]), "rf_loci.txt")
